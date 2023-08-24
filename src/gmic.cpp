cb43d5e8cf61509964698f97d1b5ca63f6de0bbb
David Tschumperle <David.Tschumperle@unicaen.fr>
2023-08-24 10:00:56 +0200

    .

--- src/gmic.cpp
+++ src/gmic.cpp
@@ -4272,7 +4272,7 @@ gmic& gmic::display_plots(CImgList<T>& images, CImgList<char>& images_names,
     CImgList<char> ncommands_line;
     unsigned int nposition = 0;
     CImg<char>::string("").move_to(callstack); // Anonymous scope
-    CImg<char>::string("_print_nolog").move_to(ncommands_line);
+    CImg<char>::string("_print_no_header").move_to(ncommands_line);
     _run(ncommands_line,nposition,images,images_names,images,images_names,variables_sizes,0,0,0,false);
     callstack.remove();
     return *this;
