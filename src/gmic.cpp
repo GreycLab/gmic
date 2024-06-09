3d6be1d3bee0a7585fcccff8ae0e11098077a1ef
David Tschumperle <David.Tschumperle@unicaen.fr>
2024-06-09 17:22:13 +0200

    Auto-commit for release 3.4.0_pre

--- src/gmic.cpp
+++ src/gmic.cpp
@@ -13070,8 +13070,8 @@ gmic& gmic::_run(const CImgList<char>& commands_line, unsigned int& position,
             const char *curr_command = "";
             for (unsigned int k = initial_callstack_size - 1; k>0 && *(curr_command = callstack[k])=='*'; --k) {}
             curr_command+=*curr_command=='+';
-            const bool run_subcommand = *curr_command && *command=='_' &&
-              !std::strncmp(command + 1,curr_command,std::strlen(curr_command));
+            const bool run_subcommand = *curr_command && *command=='_' && command[1]=='_' &&
+              !std::strncmp(command + 2,curr_command,std::strlen(curr_command));
 
             if (is_debug) {
               CImg<char> command_code_text(264);
